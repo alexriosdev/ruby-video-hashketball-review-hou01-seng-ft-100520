32ab30d0b446978f19b4bd3ec92a8af08b6f6594
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-23 06:02:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -157,7 +157,7 @@ end
 def player_numbers(team_name)
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
-      return team_data[:players].map { |player| player}
+      return team_data[:players].map { |player| player[:number]}
     end
   end
 end
