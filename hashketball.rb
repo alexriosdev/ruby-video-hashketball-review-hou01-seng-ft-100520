a6a2807d5246d46bf5283497cfe44035e6e581c3
Alejandro Rios <alexrios.dev@gmail.com>
2020-09-23 05:48:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,5 +131,6 @@ end
 def num_points_scored(player_name)
   game_hash.each do |location, team_data|
     team_data[:players].each do |player|
-      
+      if player[:player_name] == player_name
+        
 end
